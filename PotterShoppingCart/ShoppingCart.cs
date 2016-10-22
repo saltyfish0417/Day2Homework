d984d82cbdf848c639ceb126789f26ab5f41b445
鹹魚 <@FISH>
2016-10-23 02:13:37 +0800

    第二個測試案例綠燈

--- PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/ShoppingCart.cs
@@ -13,7 +13,22 @@ namespace PotterShoppingCart
 
         public void checkout(List<HarryPotter> books)
         {
-            this.OrderAmount = 100;
+            if (books.Count == 1)
+            {
+                this.OrderAmount = 100;
+            }
+            else if (books.Count == 2)
+            {
+                if (books[0].Episode.Equals(books[1].Episode))
+                {
+                    this.OrderAmount = 100 * 2;
+                }
+                else
+                {
+                    this.OrderAmount = 100 * 2 * 0.95;
+                    
+                }
+            }          
         }
     }
 }
